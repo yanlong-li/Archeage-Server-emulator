18b4877e6f0cf5c12f4fbf6cb55c0df0407a11ef
Rain0Ash <Rain0Ash@gmail.com>
2018-08-16 18:22:24 +0300

    Revert "No GG"
    This reverts commit 532e7dc67637d8694808530435cde430e4dbf1aa.

--- ArcheAgeLauncher/MainWindow.xaml.cs
+++ ArcheAgeLauncher/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace ArcheAgeLauncher
             using (var game = new Process())
             {
                 var info = new ProcessStartInfo("archeage.exe");
-                info.Arguments = string.Format(args + " -r +auth_ip {0} -uid {1} -token {2} +acpxmk",
+                info.Arguments = string.Format(args + " -r +auth_ip {0} -uid {1} -token {2}",
                     ip + ":" + port,
                     uid.Text,
                     token

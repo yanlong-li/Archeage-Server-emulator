1b8d44e1797f63b2ea7cbbcd63f93534e580643f
NL0bP <38990417+NL0bP@users.noreply.github.com>
2018-09-03 00:04:29 +0300

    fix settings

--- ArcheAgeLogin/Properties/Settings.Designer.cs
+++ ArcheAgeLogin/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ArcheAgeLogin.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -133,7 +133,7 @@ namespace ArcheAgeLogin.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("3")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
         public string ServerClientVersion {
             get {
                 return ((string)(this["ServerClientVersion"]));
